e79885673c1cc6554998ee2a10b5d95b525a529d
naterenegar <naterenegar@gmail.com>
2019-12-03 22:52:42 -0500

    This is not good rust

--- 03/3.rs
+++ 03/3.rs
@@ -38,43 +38,66 @@ fn parse_directions(s:&str) -> (Vec<(i32, i32)>, Vec<(i32, i32)>) {
     (a, b)
 }
 
-fn is_horizontal( ((c1_x, _), (c2_x, _)) : ((i32, i32), (i32, i32)) ) -> bool {
-   c1_x == c2_x
+fn is_horizontal( ((_, c1_y), (_, c2_y)) : ((i32, i32), (i32, i32)) ) -> bool {
+   c1_y == c2_y
 }
 
 fn determine_crossings(a:&Vec<(i32, i32)>, b:&Vec<(i32, i32)>) -> Vec<(i32, i32)> {
     let mut a_cp = a.clone();
     let mut crossings:Vec<(i32, i32)> = Vec::new();
-    let mut line_a = (0, a_cp.pop());
+    let mut line_a;
+
+
+    match a_cp.pop() {
+        Some(i) => line_a = ((0, 0), i),
+        None => return Vec::new()
+    }
 
     while !a_cp.is_empty() {
         let mut b_cp = b.clone();
-        let mut line_b = (0, b_cp.pop());
+        let mut line_b = ((0, 0), (0, 0));
 
-        line_a = (line_a.1, a_cp.pop());
+        match b_cp.pop() {
+            Some(i) => line_b = ((0, 0), i),
+            None => ()
+        }
 
-        while !b_cop.is_empty() {
-            line_b = (line_b.1, b_cp.pop());
+        match a_cp.pop() {
+            Some(i) => line_a = (line_a.1, i),
+            None => ()
+        }
+
+        println!("({}, {}) ({}, {})", (line_a.0).0, (line_a.0).1, (line_a.1).0, (line_a.1).1);
+
+        while !b_cp.is_empty() {
+            match b_cp.pop() {
+                Some(i) => line_b = (line_b.1, i),
+                None => ()
+            }
 
             if is_horizontal(line_a) {
                 if is_horizontal(line_b) {
-                    if ((line_a.0).1 == (line_b.0).1) {
+                    if (line_a.0).1 == (line_b.0).1 {
                         // Add all overlapping points
                     }
                 } else {
-                    if ((line_a.0).0 >= (line_b.0).0 >= (line_a.1).0
-                        || (line_a.0).0 <= (line_b.0).0 <= (line_a.1).0) {
-                        crossings.push(((line_b.0.)0, (line_a.0).1));
+                    if (((line_a.0).0 >= (line_b.0).0 && (line_b.0).0 >= (line_a.1).0)
+                        || ((line_a.0).0 <= (line_b.0).0 && (line_b.0).0 <= (line_a.1).0))
+                        && (((line_b.0).1 >= (line_a.0).1 && (line_a.0).1 >= (line_b.1).1)
+                            || ((line_b.1).1 >= (line_a.0).1 && (line_a.0).1 >= (line_b.0).1)){
+                        crossings.push(((line_b.0).0, (line_a.0).1));
                     }
                 }
             } else {
                 if is_horizontal(line_b) {
-                    if ((line_b.0).0 >= (line_a.0).0 >= (line_b.1).0
-                        || (line_b.0).0 <= (line_a.0).0 <= (line_b.1).0) {
+                    if (((line_b.0).0 >= (line_a.0).0 && (line_a.0).0 >= (line_b.1).0)
+                        || ((line_b.0).0 <= (line_a.0).0 && (line_a.0).0 <= (line_b.1).0))
+                        && (((line_a.0).1 >= (line_b.0).1 && (line_b.0).1 >= (line_a.1).1)
+                            || ((line_a.1).1 >= (line_b.0).1 && (line_b.0).1 >= (line_a.0).1)) {
                         crossings.push(((line_a.0).0, (line_b.0).1));
                     }
                 } else {
-                    if ((line_a.0).0 == (line_b.0).0) {
+                    if (line_a.0).0 == (line_b.0).0 {
                         // Add all overlapping points
                     }
                 }
@@ -83,12 +106,29 @@ fn determine_crossings(a:&Vec<(i32, i32)>, b:&Vec<(i32, i32)>) -> Vec<(i32, i32)
         }
     }
 
+    crossings
+}
 
-    Vec::new()
+fn find_min_pair_sum(pairs:&Vec<(i32, i32)>) -> i32 {
+    let mut min:i32 = 0x7FFFFFFF;
+    for (x,y) in pairs.iter() {
+        if (x.abs() + y.abs()) < min {
+            min = x.abs() + y.abs();
+        }
+    }
+
+    min
 }
 
 fn main() {
 
     let (a, b) = parse_directions("input.txt");
-    detremine_crossings(&a, &b);
+    let crossings = determine_crossings(&a, &b);
+
+    for (x,y) in crossings.iter() {
+        println!("x: {}, y: {}", x, y);
+    }
+
+    let min_dist = find_min_pair_sum(&crossings);
+    println!("Answer: {}", min_dist);
 }
